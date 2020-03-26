0624e446169023d9de494a2f7235ba423e7b0575
Marcin <m@m.pl>
2020-03-26 13:04:37 +0100

    My Django Girls app, first commit

--- strona/strona/settings.py
+++ strona/strona/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'e3goxzq49lvgf2edb5t3hkq19e-9e*ac8=t99ty$zyddt(mnuo'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['127.0.0.1', '.pythonanywhere.com']
 
 
 # Application definition
@@ -104,9 +104,9 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/3.0/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'pl-pl'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'Europe/Warsaw'
 
 USE_I18N = True
 
@@ -119,3 +119,4 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
